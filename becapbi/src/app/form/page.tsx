0abc05762f1c5a133c54d92da536b283c6c96c1b
jhoestar <202202883@est.umss.edu>
2024-10-26 22:33:53 -0400

    typography

--- becapbi/src/app/form/page.tsx
+++ becapbi/src/app/form/page.tsx
@@ -1,12 +1,16 @@
 import Header from "@/app/form/components/Header"
+import { TypographyH1 } from "../../components/ui/TypographyH1"
 
 
 const PBIForm = () => {
   return (
     <>
         <Header/>
-        
-
+        <main className="p-4">
+          <TypographyH1>
+              Solicitud de Beca PBI
+          </TypographyH1>
+        </main>
     </>
   )
 }
