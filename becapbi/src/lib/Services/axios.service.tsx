7f76a89908ef10c22fb5024b21bfaec3d80a02fa
jhoestar <202202883@est.umss.edu>
2024-11-12 11:31:56 -0400

    axios service

--- becapbi/src/lib/Services/axios.service.tsx
+++ becapbi/src/lib/Services/axios.service.tsx
@@ -1,5 +1,10 @@
 import axios from "axios"
 
+
+export const AxiosServiceCiclo = () => {
+  return axios.get('http://sispos.dev.umss.net/api/postulacion/ciclo-formulario')
+}
+
 export const AxiosServiceLogin = (data: {}) => {
   return axios.post('http://sispos.dev.umss.net/api/postulante/login', data)
 }
