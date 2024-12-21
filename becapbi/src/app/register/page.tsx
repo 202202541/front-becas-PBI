df133253b8b096fa55dd88f8b6290d04a42238a3
jhoestar <202202883@est.umss.edu>
2024-12-21 19:15:09 -0400

    register modificado

--- becapbi/src/app/register/page.tsx
+++ becapbi/src/app/register/page.tsx
@@ -1,9 +1,17 @@
 import FormRegister from "@/app/components/FormRegister"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 const Register = () => {
   return (
-    <div className="flex w-full items-center justify-center py-8">
-      <FormRegister />
+    <div className="py-10">
+      <Card className="w-11/12 max-w-xl mx-auto bg-[#F3F4F7]">
+        <CardHeader>
+          <CardTitle className="text-2xl text-center">Registro de Cuenta</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <FormRegister />
+        </CardContent>
+      </Card>
     </div>
   )
 }
